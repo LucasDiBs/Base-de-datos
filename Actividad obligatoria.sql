423eaef3ccf5e00c5819141385eed01280b9dbcf
Lucas Di Benedetto <lucas_dibenedetto@hotmail.com>
2022-06-09 21:34:18 -0300

    Base de datos para el tp

---
+++ Actividad obligatoria.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from usuario
+LIMIT 0, 1000
+
+-- Date: 2022-06-09 21:17
+*/
+INSERT INTO `` (`id`,`nombre`,`apelido`,`edad`,`fecha`,`provincia`) VALUES (1,'BRENDA','PEREZ',24,'2022-06-09 21:08:16','LA PAMPA');
+INSERT INTO `` (`id`,`nombre`,`apelido`,`edad`,`fecha`,`provincia`) VALUES (2,'LUCAS','CASTRO',18,'2022-06-09 21:08:16','SALTA');
+INSERT INTO `` (`id`,`nombre`,`apelido`,`edad`,`fecha`,`provincia`) VALUES (3,'JOAUIN','VELEZ',15,'2022-06-09 21:08:16','SAN LUIS');
+INSERT INTO `` (`id`,`nombre`,`apelido`,`edad`,`fecha`,`provincia`) VALUES (4,'RODRIGO','RODRIGUEZ',27,'2022-06-09 21:08:16','BUENOS AIRES');
+INSERT INTO `` (`id`,`nombre`,`apelido`,`edad`,`fecha`,`provincia`) VALUES (5,'SUSANA','MARQUEZ',15,'2022-06-09 21:08:16','CHUBUT');
